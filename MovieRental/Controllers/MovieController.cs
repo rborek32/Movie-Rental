2fb79f04ed9c6f1180c87f124ee760dc3440e9ad
Rafal Borek <rafal.borek@ihsmarkit.com>
2023-08-01 09:17:14 +0200

    Init backend

---
+++ MovieRental/Controllers/MovieController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRental.Controllers
+{
+    [Route("api/movies/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+    }
+}
